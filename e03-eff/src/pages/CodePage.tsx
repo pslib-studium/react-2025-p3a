6a82af30b7f0ccccbe3f925771ddffc0756f0433
Michal Stehlík <michal.stehlik@pslib.cz>
2025-09-23 14:20:09 +0200

    wip 03

--- e03-eff/src/pages/CodePage.tsx
+++ e03-eff/src/pages/CodePage.tsx
@@ -10,6 +10,9 @@ const Page: React.FC<CodePageProps> = ({navAction}) => {
     const handleChangeCode = () => {
         setCode(x => x + ".")
     }
+    const checkCode = () => {
+        console.log(">", code);
+    }
     useEffect(() => { 
         console.log("mount");
         return () => {
@@ -17,11 +20,32 @@ const Page: React.FC<CodePageProps> = ({navAction}) => {
         }
     }, []);
     useEffect(() => {
-        console.log("code change");
+        console.log("code change", code);
         return () => {
             console.log("unmount 2");
         }
     }, [code]);
+    useEffect(() => {
+        const onKeyDown = (e: KeyboardEvent) => {
+            const k = e.key;
+            console.log("State:",k,code);
+            if (k === "Enter") {
+                checkCode();
+                console.log("X", k, code);
+                if (code === "12345") {
+                    console.log("GO", code, k);
+                    navAction("success");
+                }
+            }
+            else {
+                setCode(x => x + k);
+            }   
+        }
+        window.addEventListener("keydown", onKeyDown);
+        return (() => {
+            window.removeEventListener("keydown", onKeyDown);
+        });
+    }, [])
     console.log("render");
     return (
         <>
